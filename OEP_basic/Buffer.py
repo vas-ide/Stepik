bc4077b813f2169618e4ea6d7b93622b52a450be
vas-ide <vas.ide2022@gmail.com>
2022-08-02 08:27:54 +0300

    Stepik:
             OEP - UPD

--- OEP_basic/Buffer.py
+++ OEP_basic/Buffer.py
@@ -1,8 +1,32 @@
 
 
+class Buffer:
 
+    def __init__(self):
+        self.buffer_lst = []
 
+    def add(self, *a):
+        for i in a:
+            self.buffer_lst.append(i)
+            if len(self.buffer_lst) == 5:
+                sum_lst = 0
+                for j in self.buffer_lst:
+                    sum_lst += j
+                print(sum_lst)
+                self.buffer_lst = []
 
+    def get_current_part(self):
+        return self.buffer_lst
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
 
 
 
