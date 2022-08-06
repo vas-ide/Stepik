b879a2d87f293f09065367a30f5e2dac141964a5
vas-ide <vas.ide2022@gmail.com>
2022-08-06 13:12:09 +0300

    Stepik:
             Class_from_class.py-init

--- OOP_basic/Class_from_class.py
+++ OOP_basic/Class_from_class.py
@@ -17,8 +17,53 @@ print(x.even_length())
 x.append(6)
 print(x.even_length())
 
+class EvenLengthMixin:
+    def even_length(self):
+        return len(self) % 2 == 0
 
+class MyList(list, EvenLengthMixin):
+    def pop(self):
+        x = super(MyList, self).pop()
+        print("last value is", x)
+        return x
 
+print(MyList.mro())
+x = MyList([1, 2, 3])
+print(x.even_length())
+x.append(4)
+print(x.even_length())
+
+ml = MyList([1, 2, 4, 17])
+z = ml.pop()
+print(z)
+print(ml)
+
+class MyDict(dict, EvenLengthMixin):
+    pass
+
+x = MyDict()
+x["key"] = "value"
+x["another key"] = "another value"
+print(x.even_length())
+
+#__________ПРИМЕР КОТОПЁС__________
+class Cat():
+   def say(self,times):
+      print('Meow '*times)
+class Dog():
+   def say(self,times):
+      print('Bow-Wow '*times)
+class CatDog(Cat,Dog):
+   pass
+
+class DogCat(Dog,Cat):
+   pass
+
+muteDog=CatDog()
+muteDog.say(3)       #Meow Meow Meow
+
+muteCat=DogCat()
+muteCat.say(2)       #Bow-Wow Bow-Wow
 
 
 
